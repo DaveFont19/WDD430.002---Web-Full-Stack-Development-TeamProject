9ab0d4b754d100066c28448fc56f13f980f23839
Andrew Riley <andrewvriley@gmail.com>
2026-03-26 20:11:04 -0600

    Completed Nav Links

---
+++ wdd430-team-project/src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <div >
+            <div id="Hero">
+                <p>Put Hero Here</p>
+            </div>
+            <div>
+                <p>Cart page</p>
+            </div>
+        </div>
+    );
+}
---
+++ wdd430-team-project/src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <div >
+            <div id="Hero">
+                <p>Put Hero Here</p>
+            </div>
+            <div>
+                <p>Profile page</p>
+            </div>
+        </div>
+    );
+}
